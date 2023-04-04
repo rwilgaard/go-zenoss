746680999ecbda497a899c40bdf4338d239f057e
rwilgaard <rasmus@wilgaard.dk>
2023-04-04 19:56:20 +0200

    feat: Add DeviceEventList and DeviceEventCount types

--- device.go
+++ device.go
@@ -5,24 +5,38 @@ import (
 )
 
 type Device struct {
-    Name            string                    `json:"name,omitempty"`
-    UID             string                    `json:"uid,omitempty"`
-    ProductionState int                       `json:"productionState,omitempty"`
-    Systems         []map[string]string       `json:"systems,omitempty"`
-    Groups          []map[string]string       `json:"groups,omitempty"`
-    Collector       string                    `json:"collector,omitempty"`
-    IpAddress       float64                   `json:"ipAddress,omitempty"`
-    IpAddressString string                    `json:"ipAddressString,omitempty"`
-    SerialNumber    string                    `json:"serialNumber,omitempty"`
-    PythonClass     string                    `json:"pythonClass,omitempty"`
-    HwManufacturer  map[string]string         `json:"hwManufacturer,omitempty"`
-    OsModel         map[string]string         `json:"osmodel,omitempty"`
-    Priority        int                       `json:"priority,omitempty"`
-    HwModel         map[string]string         `json:"hwModel,omitempty"`
-    TagNumber       string                    `json:"tagNumber,omitempty"`
-    OsManufacturer  map[string]string         `json:"osManufacturer,omitempty"`
-    Location        interface{}               `json:"location,omitempty"`
-    Events          map[string]map[string]int `json:"events,omitempty"`
+    Name            string              `json:"name,omitempty"`
+    UID             string              `json:"uid,omitempty"`
+    ProductionState int                 `json:"productionState,omitempty"`
+    Systems         []map[string]string `json:"systems,omitempty"`
+    Groups          []map[string]string `json:"groups,omitempty"`
+    Collector       string              `json:"collector,omitempty"`
+    IpAddress       float64             `json:"ipAddress,omitempty"`
+    IpAddressString string              `json:"ipAddressString,omitempty"`
+    SerialNumber    string              `json:"serialNumber,omitempty"`
+    PythonClass     string              `json:"pythonClass,omitempty"`
+    HwManufacturer  map[string]string   `json:"hwManufacturer,omitempty"`
+    OsModel         map[string]string   `json:"osmodel,omitempty"`
+    Priority        int                 `json:"priority,omitempty"`
+    HwModel         map[string]string   `json:"hwModel,omitempty"`
+    TagNumber       string              `json:"tagNumber,omitempty"`
+    OsManufacturer  map[string]string   `json:"osManufacturer,omitempty"`
+    Location        interface{}         `json:"location,omitempty"`
+    Events          *DeviceEventList    `json:"events,omitempty"`
+}
+
+type DeviceEventList struct {
+    Clear    *DeviceEventCount `json:"clear,omitempty"`
+    Critical *DeviceEventCount `json:"critical,omitempty"`
+    Error    *DeviceEventCount `json:"error,omitempty"`
+    Warning  *DeviceEventCount `json:"warning,omitempty"`
+    Info     *DeviceEventCount `json:"info,omitempty"`
+    Debug    *DeviceEventCount `json:"debug,omitempty"`
+}
+
+type DeviceEventCount struct {
+    AcknowledgedCount int `json:"acknowledged_count,omitempty"`
+    Count             int `json:"count,omitempty"`
 }
 
 type GetDevicesList struct {
@@ -42,9 +56,9 @@ type GetDevicesQuery struct {
     Start  int                    `json:"start,omitempty"`  // (optional) Offset to return the results from; used in pagination (default: 0)
     Params map[string]interface{} `json:"params,omitempty"` // (optional) Key-value pair of filters for this search. Can be one of the following: name, ipAddress, deviceClass, or productionState (default: None)
     Keys   []string               `json:"keys,omitempty"`
-    Limit  int                    `json:"limit,omitempty"`  // (optional) Number of items to return; used in pagination (default: 50)
-    Sort   string                 `json:"sort,omitempty"`   // (optional) Number of items to return; used in pagination (default: 50)
-    Dir    string                 `json:"dir,omitempty"`    // (optional) Sort order; can be either 'ASC' or 'DESC' (default: 'ASC')
+    Limit  int                    `json:"limit,omitempty"` // (optional) Number of items to return; used in pagination (default: 50)
+    Sort   string                 `json:"sort,omitempty"`  // (optional) Number of items to return; used in pagination (default: 50)
+    Dir    string                 `json:"dir,omitempty"`   // (optional) Sort order; can be either 'ASC' or 'DESC' (default: 'ASC')
 }
 
 func (a *API) GetDevices(query GetDevicesQuery) (*GetDevicesResult, *http.Response, error) {
